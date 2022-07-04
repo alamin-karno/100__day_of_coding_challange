5b0b8708d2401956ce92a707a2bcb34e27f4757b
alamin-karno <amin15-1951@diu.edu.bd>
2022-07-04 09:25:34 +0600

    Biryani Classes

---
+++ Day 04/BIRYANI.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int testCase, weeks, coins;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		testCase = sc.nextInt();
+		
+		while(testCase > 0){
+		    weeks = sc.nextInt();
+		    coins = sc.nextInt();
+		    
+		    System.out.println(weeks * coins);
+		    
+		    testCase--;
+		}
+	}
+}
