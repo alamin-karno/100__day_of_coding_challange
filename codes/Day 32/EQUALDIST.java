c14ef2a0ae31a7999289b5029d89646b8e1681da
alamin-karno <amin15-1951@diu.edu.bd>
2022-08-01 08:49:46 +0600

    Day 32: Equal Distribution

---
+++ codes/Day 32/EQUALDIST.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int testCase, alice, bob;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		testCase = sc.nextInt();
+		
+		while(testCase > 0){
+		    alice = sc.nextInt();
+		    
+		    bob = sc.nextInt();
+		    
+		    if((alice+bob) % 2 == 0){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		    
+		    testCase --;
+		}
+	}
+}
