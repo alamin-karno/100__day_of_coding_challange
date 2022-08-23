f40f8be3563778eadb29659a2ffd9968ce1e375a
alamin-karno <amin15-1951@diu.edu.bd>
2022-08-23 13:31:45 +0600

    Day 54: Monthly Budget

---
+++ codes/Day 54/BUDGET_.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int testCase, totalRupee, dailyUse;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		testCase = sc.nextInt();
+		
+		while(testCase -- > 0){
+		    totalRupee = sc.nextInt();
+		    
+		    dailyUse = sc.nextInt();
+		    
+		    if((dailyUse * 30) <= totalRupee){
+		        System.out.println("YES");
+		    }else{
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
