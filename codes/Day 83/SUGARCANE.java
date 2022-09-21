f782a09ac54c35fbb861b0d4c5c330d3da157485
alamin-karno <amin15-1951@diu.edu.bd>
2022-09-21 13:41:27 +0600

    Day 83: Sugarcane Juice Business

---
+++ codes/Day 83/SUGARCANE.java
@@ -0,0 +1,26 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int testCase, glassesOfSugarCane;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		testCase = sc.nextInt();
+		
+		while(testCase -- > 0){
+		    glassesOfSugarCane = sc.nextInt();
+		    
+		    int profit = (glassesOfSugarCane*50) * 30/100;
+		    System.out.println(profit);
+		}
+	}
+}
